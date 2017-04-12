be7dd567a6a538d2492ea68fd2bd965d8c99c2c6
theClubhou.se <heythere@theclubhou.se>
2017-04-12 13:28:05 -0400

    Backend Day2

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -11,13 +11,17 @@ class ArticlesController extends Controller
     //will get list of Articles
     public function index()
     {
+      //create a query to get a list of articles and receive them on the Frontend
+      $articles = Article::all();
 
+      return Response::json($articles);
     }
 
     //store article - takes request param from frontend
     public function store(Request $request)
     {
       $article = new Article;
+
       $article->title = $request->input('title');
       $article->body = $request->input('body');
 
@@ -37,19 +41,36 @@ class ArticlesController extends Controller
     //update function - 2 params id & request
     public function update($id, Request $request)
     {
+      $article = Article::find($id);
 
+      $article->title = $request->input('title');
+      $article->body = $request->input('body');
+
+      $image = $request->file('image');
+      $imageName = $image->getClientOriginalName();
+      $image->move("storage/", $imageName);
+      $article->image = $request->root()."/storage/".$imageName;
+      $article->save();
+
+      return Response::json(["success" => "Article Has Been Updated!"]);
     }
 
     //shows individal article
     public function show($id)
     {
+      $article = Article::find($id);
 
+      return Response::json($article);
     }
 
     //delete function to delete a single article
     public function destroy($id)
     {
+      $article = Article::find($id);
 
+      $article->delete();
+
+      return Response::json(["success" => "Article Deleted."]);
     }
 
 
